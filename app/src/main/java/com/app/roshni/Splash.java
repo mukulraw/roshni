f5a24563bb41e29c72bd650caf2f9d135db35a24
mukul <mukulraw199517@gmail.com>
2019-06-27 13:46:57 +0530

    started

---
+++ app/src/main/java/com/app/roshni/Splash.java
@@ -0,0 +1,33 @@
+package com.app.roshni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+
+    Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(Splash.this , SignupLogin.class);
+                startActivity(intent);
+                finish();
+
+            }
+        } , 1200);
+
+    }
+}
