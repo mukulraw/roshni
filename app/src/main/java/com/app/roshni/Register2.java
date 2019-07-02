9b11f040327342849fc9aeea1bae4dc75e33dc07
mukul <mukulraw199517@gmail.com>
2019-07-02 18:14:02 +0530

    started

---
+++ app/src/main/java/com/app/roshni/Register2.java
@@ -0,0 +1,68 @@
+package com.app.roshni;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class Register2 extends AppCompatActivity {
+
+    TabLayout tabs;
+    CustomViewPager pager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register2);
+
+        tabs = findViewById(R.id.tabLayout2);
+        pager = findViewById(R.id.pager);
+
+        tabs.addTab(tabs.newTab().setText("COMPANY"));
+
+
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+        tabs.setupWithViewPager(pager);
+
+        pager.setPagingEnabled(false);
+
+        LinearLayout tabStrip = ((LinearLayout)tabs.getChildAt(0));
+        tabStrip.setEnabled(false);
+        for(int i = 0; i < tabStrip.getChildCount(); i++) {
+            tabStrip.getChildAt(i).setClickable(false);
+        }
+
+        tabs.getTabAt(0).setText("COMPANY");
+
+    }
+
+    class PagerAdapter extends FragmentStatePagerAdapter
+    {
+
+
+
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+                return new brand();
+
+
+        }
+
+        @Override
+        public int getCount() {
+            return 1;
+        }
+    }
+
+}
